0ced1a40f98df3742041391f5da6c340bacbfdf7
Travis Hunt <tdhunt631@gmail.com>
2012-10-27 21:16:53 -0600

    Change client to windows form. Added icon and system tray functionality.

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,30 +6,43 @@ using System.Xml.Serialization;
 using System.IO;
 using Common;
 using System.Threading;
+using System.Windows.Forms;
 
-namespace SuperSimpleSync
+namespace SuperSimpleSync 
 {
-    class Program
+    class Program : System.Windows.Forms.Form
     {
-        private DirectoryInfo LocalStorage = new System.IO.DirectoryInfo(@"C:\temp\TestSyncDir");
-        private Guid accountId = Guid.Parse("{FC948776-0FA5-4ABC-A2F3-E8AC8005DFCA}");
+        private static DirectoryInfo LocalStorage = new System.IO.DirectoryInfo(@"C:\temp\TestSyncDir");
+        private static Guid accountId = Guid.Parse("{FC948776-0FA5-4ABC-A2F3-E8AC8005DFCA}"); 
+        private System.Windows.Forms.ContextMenuStrip trayMenu;
+        private System.Windows.Forms.NotifyIcon tray;
+        private Label label1;
+        private System.ComponentModel.IContainer components;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        FormWindowState lastState = new FormWindowState();
         SyncServer.Sync _sync = new SyncServer.Sync();
 
-        static void Main(string[] args)
-        {
+        static void Main() 
+        {            
             Program p = new Program();
+            p.InitializeComponent();
+            Application.Run(p);
+        }
+
+        public Program()
+        {
             try
             {
-                p.SyncWithServer();
+                SyncWithServer();
             }
             catch (Exception ex)
             {
                 Console.WriteLine("There was an error syncing: " + ex.Message);
                 Environment.Exit(1);
             }
-            Environment.Exit(0);
         }
-
+                
         private void SyncWithServer()
         {
             SyncDir local = Util.AuditTree(LocalStorage);
@@ -60,7 +73,6 @@ namespace SuperSimpleSync
                 }
             }
         }
-
         
         private void ResolveDifferencesWithLocal(SyncDir diff, DirectoryInfo dir)
         {
@@ -82,6 +94,104 @@ namespace SuperSimpleSync
                     ResolveDifferencesWithLocal(d, sub);
                 }
             }
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Program));
+            this.label1 = new System.Windows.Forms.Label();
+            this.trayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.trayMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Interface needs development";
+            // 
+            // trayMenu
+            // 
+            this.trayMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.trayMenu.Name = "trayMenu";
+            this.trayMenu.Size = new System.Drawing.Size(153, 70);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // tray
+            // 
+            this.tray.BalloonTipText = "SuperSimpleSync is still running!";
+            this.tray.ContextMenuStrip = this.trayMenu;
+            this.tray.Icon = ((System.Drawing.Icon)(resources.GetObject("tray.Icon")));
+            this.tray.Text = "SuperSimpleSync";
+            this.tray.DoubleClick += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // Program
+            // 
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Program";
+            this.ShowInTaskbar = false;
+            this.Text = "SuperSimpleSync";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Program_FormClosed);
+            this.Resize += new System.EventHandler(this.Program_Resize);
+            this.trayMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Program_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.tray.Visible = false;
+        }
+
+        private void Program_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                this.tray.Visible = true;
+                this.tray.ShowBalloonTip(250);
+                this.Hide();
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = this.lastState;
+            this.tray.Visible = false;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            Environment.Exit(1);
         }
     }
 }
